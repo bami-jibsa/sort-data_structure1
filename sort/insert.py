540d8873ec7494ba3e12a7ef369a1f03c6369633
bami-jibsa <tlswjddn0615@naver.com>
2024-04-12 05:48:32 +0900

    dffs.py

--- sort/insert.py
+++ sort/insert.py
@@ -5,7 +5,7 @@ for i in range(1, len(n)):
     for j in range(i, 0, -1):
         if n[j-1] > n[j]:
             n[j-1], n[j] = n[j], n[j-1]    
-            print(n)        
+            print(n)
 
 print(n)
 # #
